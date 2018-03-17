aa502f16632f855eb4df4bda1c07f37c5f778314
myrovh <myrovh@yandex.com>
2018-03-17 18:40:39 +1100

    setup configs for debugging

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
 const APP = () => {
   return (
     <div>
       <p>React here!</p>
-      <p>React here! Again</p>
-      <p>React here! Again</p>
-      <p>React here! Even More</p>
     </div>
   );
 };
+
 ReactDOM.render(<APP />, document.getElementById('app'));
