bec447922910fa8133a15e1b26943414f02e3ec0
myrovh <myrovh@yandex.com>
2018-03-22 15:32:10 +1100

    enable hot reloading for react components

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {hot} from 'react-hot-loader';
 import {Clock} from './components/Clock';
 import {Welcome} from './components/Welcome';
 
@@ -11,4 +12,6 @@ const app = () => {
   );
 };
 
-export {app as App};
+const hotApp = hot(module)(app);
+
+export {hotApp as App};
