d87026bd617079956becabd91c8965a5e6ff00e5
Unknown <myrovh@yandex.com>
2018-03-28 16:49:50 +1100

    add app drawer

--- src/Application.tsx
+++ src/Application.tsx
@@ -2,6 +2,7 @@ import {Card, CardContent, CssBaseline, Grid, Typography} from 'material-ui';
 import * as React from 'react';
 import {hot} from 'react-hot-loader';
 import {Clock} from './components/Clock';
+import {AppDrawer} from './components/Drawer';
 import {
   ClassesFancyButton,
   ClassNamesFancyButton,
@@ -12,30 +13,32 @@ const app = () => {
   /* Padding is set on div to account for the negative margin of the grid.
   (default spacing for grid items is 8 */
   return (
-    <div style={{padding: 16}}>
+    <div>
       <CssBaseline />
-      <Grid container={true}>
-        <Grid item={true} xs={3}>
-          <Welcome name="User" />
+      <AppDrawer>
+        <Grid container={true}>
+          <Grid item={true} xs={3}>
+            <Welcome name="User" />
+          </Grid>
+          <Grid item={true} xs={true}>
+            <Clock />
+          </Grid>
+          <Grid item={true} xs={true}>
+            <Card>
+              <CardContent>
+                <Typography variant="body1">
+                  Modify style using classNames method
+                </Typography>
+                <ClassNamesFancyButton />
+                <Typography variant="body1">
+                  Modify style using classes method
+                </Typography>
+                <ClassesFancyButton />
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
-        <Grid item={true} xs={true}>
-          <Clock />
-        </Grid>
-        <Grid item={true} xs={true}>
-          <Card>
-            <CardContent>
-              <Typography variant="body1">
-                Modify style using classNames method
-              </Typography>
-              <ClassNamesFancyButton />
-              <Typography variant="body1">
-                Modify style using classes method
-              </Typography>
-              <ClassesFancyButton />
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
+      </AppDrawer>
     </div>
   );
 };
---
+++ src/components/Drawer.tsx
@@ -0,0 +1,81 @@
+import {
+  AppBar,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+  withStyles,
+} from 'material-ui';
+import {Clock, GestureTap} from 'mdi-material-ui';
+import * as React from 'react';
+
+const drawerWidth = 240;
+
+const decorate = withStyles(({palette, spacing, mixins}) => ({
+  appBar: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+  },
+  content: {
+    backgroundColor: palette.background.default,
+    flexGrow: 1,
+    padding: spacing.unit * 3,
+  },
+  drawerPaper: {
+    position: 'relative' as 'relative',
+    width: drawerWidth,
+  },
+  root: {
+    display: 'flex',
+    flexGrow: 1,
+    overflow: 'hidden' as 'hidden',
+    position: 'relative' as 'relative',
+    width: '100%',
+    zIndex: 1,
+  },
+  toolbar: mixins.toolbar,
+}));
+
+interface IDrawerProps {
+  children?: React.ReactNode;
+}
+
+const decoratedDrawer = decorate<IDrawerProps>(({children, classes}) => (
+  <div className={classes.root}>
+    <AppBar position="absolute" className={classes.appBar}>
+      <Toolbar>
+        <Typography variant="title" color="inherit">
+          Permanent drawer
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Drawer variant="permanent" classes={{paper: classes.drawerPaper}}>
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        <ListItem button={true}>
+          <ListItemIcon>
+            <Clock />
+          </ListItemIcon>
+          <ListItemText primary="Clock" />
+        </ListItem>
+        <ListItem button={true}>
+          <ListItemIcon>
+            <GestureTap />
+          </ListItemIcon>
+          <ListItemText primary="Buttons" />
+        </ListItem>
+      </List>
+    </Drawer>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
+      {children ? children : 'Empty'}
+    </main>
+  </div>
+));
+
+export {decoratedDrawer as AppDrawer};
