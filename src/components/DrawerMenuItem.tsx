62cc65020ec06b72aee4f6e27fedfee97aaa7ad2
Unknown <myrovh@yandex.com>
2018-04-02 16:01:30 +1000

    simplify drawermenuitem
    extending ListItemProps still not solved

--- src/components/DrawerMenuItem.tsx
+++ src/components/DrawerMenuItem.tsx
@@ -3,41 +3,23 @@ import {ListItem, ListItemIcon, ListItemText} from 'material-ui';
 import * as React from 'react';
 import {NavLink} from 'react-router-dom';
 
-interface IMenuItemProps {
+interface IDrawerMenuItemProps {
   icon: React.ReactElement<any>;
   primary: React.ReactNode;
   secondary?: React.ReactNode;
-}
-
-const MenuItem: React.SFC<IMenuItemProps> = props => {
-  return (
-    <ListItem button={true}>
-      <ListItemIcon>{props.icon}</ListItemIcon>
-      <ListItemText primary={props.primary} secondary={props.secondary} />
-    </ListItem>
-  );
-};
-
-interface IDrawerMenuItemProps {
   to: H.LocationDescriptor;
 }
 
-export class DrawerMenuItem extends React.Component<
-  IMenuItemProps & IDrawerMenuItemProps
-> {
+export class DrawerMenuItem extends React.Component<IDrawerMenuItemProps> {
   public render() {
-    const menuItem = (
-      <MenuItem
-        primary={this.props.primary}
-        secondary={this.props.secondary}
-        icon={this.props.icon}
-      />
-    );
-
     return (
-      <NavLink to={this.props.to} style={{textDecoration: 'none'}}>
-        {menuItem}
-      </NavLink>
+      <ListItem component={NavLink} to={this.props.to} button={true}>
+        <ListItemIcon>{this.props.icon}</ListItemIcon>
+        <ListItemText
+          primary={this.props.primary}
+          secondary={this.props.secondary}
+        />
+      </ListItem>
     );
   }
 }
