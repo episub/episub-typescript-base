aeefa0daf975c1c52df2bb7705a498e51051e6b0
myrovh <myrovh@yandex.com>
2018-04-03 17:16:46 +1000

    move ShellBase into its own file

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,12 +1,10 @@
 import {CssBaseline} from 'material-ui';
 import {inject, Provider} from 'mobx-react';
-import {HistoryAdapter, RouterView, RouterViewProps} from 'mobx-state-router';
+import {HistoryAdapter, RouterView} from 'mobx-state-router';
 import * as React from 'react';
 import {hot} from 'react-hot-loader';
 import {AppDrawer} from './components/Drawer';
-import {ButtonPage} from './pages/Buttons';
-import {ClockPage} from './pages/Clock';
-import {HomePage} from './pages/Home';
+import {RouterShell} from './pages/RouterShell';
 import {RootStore} from './stores/root.store';
 import {history} from './utils/history';
 
@@ -17,32 +15,13 @@ const rootStore = new RootStore();
 const historyAdapter = new HistoryAdapter(rootStore.routerStore, history);
 historyAdapter.observeRouterStateChanges();
 
-const viewMap = {
-  buttons: <ButtonPage />,
-  clock: <ClockPage />,
-  home: <HomePage />,
-};
-
-@inject('rootStore')
-class ShellBase extends React.Component {
-  public render(): JSX.Element {
-    const {rootStore: {routerStore}} = this.props;
-
-    return (
-      <div>
-        <RouterView routerStore={routerStore} viewMap={viewMap} />
-      </div>
-    );
-  }
-}
-
 const app: React.SFC = () => {
   return (
     <Provider rootStore={rootStore}>
       <div>
         <CssBaseline />
         <AppDrawer>
-          <ShellBase />
+          <RouterShell />
         </AppDrawer>
       </div>
     </Provider>
